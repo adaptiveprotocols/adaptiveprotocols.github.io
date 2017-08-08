9fc8980f17d42e7acea781d1f16dfe68061ce8de
Jesse Rogers <jesse@jesserogers.net>
2017-08-07 17:19:30 -0700

    Built About Page
    Made significant progress on About page

--- _site/assets/js/adaptiva.js
+++ _site/assets/js/adaptiva.js
@@ -53,4 +53,29 @@ jQuery(document).ready(function($) { //safety pants!
   $('.tertiary-nav-content.' + navID).show().addClass('is-visible');
 });
 
+  $('.slider-dot').click(function(){
+	var $this = $(this);
+  var slideID = $this.attr('id');
+  $(this).closest('.slider').find('.slider-dot').removeClass('is-selected');
+  $this.addClass('is-selected');
+  $(this).closest('.slider').find('.slide').removeClass('is-visible').hide();
+  $('.slide#' + slideID).addClass('is-visible').show();
+});
+$('.slider-arrow.prev').click(function(){
+	var $this = $(this);
+  var slideID = $this.parent('.slide').prev('.slide').attr('id');
+  $(this).closest('.slider').find('.slide').removeClass('is-visible').hide();
+  $this.parent('.slide').prev('.slide').addClass('is-visible').show();
+  $(this).closest('.slider').find('.slider-dot').removeClass('is-selected');
+  $('.slider-dot#' + slideID).addClass('is-selected');
+});
+$('.slider-arrow.next').click(function(){
+	var $this = $(this);
+  var slideID = $this.parent('.slide').next('.slide').attr('id');
+  $(this).closest('.slider').find('.slide').removeClass('is-visible').hide();
+  $this.parent('.slide').next('.slide').addClass('is-visible').show();
+  $(this).closest('.slider').find('.slider-dot').removeClass('is-selected');
+  $('.slider-dot#' + slideID).addClass('is-selected');
+});
+
 });
--- assets/js/adaptiva.js
+++ assets/js/adaptiva.js
@@ -4,4 +4,5 @@ jQuery(document).ready(function($) { //safety pants!
   {% include js/smooth-scroll.js %}
   {% include js/nav.js %}
   {% include js/tertiary.js %}
+  {% include js/slider.js %}
 });
