e74f39b46678dc1e2d3bafbbf709d9dba61ef63b
Jesse Rogers <jesse@jesserogers.net>
2017-10-16 16:26:23 -0700

    Added Academy entry
    Also changed some comments in a script, nbd

--- _includes/js/smooth-scroll.js
+++ _includes/js/smooth-scroll.js
@@ -1,7 +1,7 @@
 $('a[href*="#"]') // Select all links with hashes
-  // Remove links that don't actually link to anything
+  // Remove links that I don't want scrolling the page
   .not('[href="#"]')
-  .not('[href="#0"]')
+  .not('[href="#0"]') // placeholder links
   .click(function(event) {
     // On-page links
     if (
