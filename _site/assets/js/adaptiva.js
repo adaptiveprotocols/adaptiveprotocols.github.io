180c4dad17f638e11b543137f4430374f5353d44
Jesse Rogers <jesse@jesserogers.net>
2017-10-17 10:35:46 -0700

    Updated script
    Added comments and other meaningless tinkering

--- _site/assets/js/adaptiva.js
+++ _site/assets/js/adaptiva.js
@@ -100,10 +100,13 @@ $('.js-nav-trigger').click(function(){
 });
 
   $(function() { // safety pants
+
   $('.tertiary-nav-item a').click(function(event){ // user clicks on tertiary nav item
-    event.preventDefault();
-    var $this = $(this),
-        contentID = $this.attr('href');
+    event.preventDefault(); // stop from scrolling/jumping
+
+    var $this = $(this);
+    var contentID = $this.attr('href');
+
     $this.closest('ul').children() // localize
       .removeClass('is-selected'); // remove mod class from all
     $this.parent().toggleClass('is-selected'); // toggle mod class on clicked item
@@ -117,12 +120,14 @@ $('.js-nav-trigger').click(function(){
     $this.parent() // select parent ul element
       .scrollTo('.is-selected', 600); // scroll to clicked element, animated 600ms
   });
-  var hash = $.trim( window.location.hash );
-  if (hash) {
-    $('.tertiary-nav-item a[href$="'+hash+'"]').trigger('click');
+
+  var hash = $.trim( window.location.hash ); // get hash value from URL
+
+  if (hash) { // if hash in the URL
+    $('.tertiary-nav-item a[href$="'+hash+'"]').click(); // find tertiary link that matches hash and click it on page load
     $('html, body')
-      .animate({scrollTop:$(window.location.hash)
-        .offset().top - 288 }, 0);
+      .animate({scrollTop:$(window.location.hash) // scroll to anchor
+        .offset().top - 288 }, 0); // offset by 288px
   }
 });
 
